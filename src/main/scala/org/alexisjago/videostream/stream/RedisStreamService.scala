f47d4ec4f2ceb91417e322b06386fe81c441318b
Alexis Jago <alexis@alexisjago.org>
2019-07-22 00:10:28 +0100

    Updated Redis Service to prevent concurrent access leading to more than 3 streams

--- src/main/scala/org/alexisjago/videostream/stream/RedisStreamService.scala
+++ src/main/scala/org/alexisjago/videostream/stream/RedisStreamService.scala
@@ -8,10 +8,14 @@ import scala.util.{Failure, Success, Try}
 
 class RedisStreamService(keyPrefix: String = "stream_") extends StreamService {
 
-  private val client = new RedisClient("localhost", 6379)
+  private val redisHost = "localhost"
+  private val redisPort = 6379
+  
   private val rnd = scala.util.Random
 
   override def getNumberOfStreams(userId: Long): Int = {
+    val client = new RedisClient(redisHost, redisPort)
+
     client.lrange[Long](s"$keyPrefix$userId", 0, -1) match {
       case None => 0
       case Some(l) => l.flatten.length
@@ -20,22 +24,32 @@ class RedisStreamService(keyPrefix: String = "stream_") extends StreamService {
 
   override def startStream(userId: Long): Try[Long] = {
 
+    val client = new RedisClient(redisHost, redisPort)
     val key = s"$keyPrefix$userId"
 
+    client.watch(key)
     val streams = client.llen(key).getOrElse(0L).toInt
 
-    if (streams >= 3)
+    // Use below line to manually test concurrency functionality
+//    Thread.sleep((5000))
+
+    if (streams >= 3) {
+      client.unwatch()
       Failure(MaxStreamsException())
+    }
     else {
       val streamId = rnd.nextLong(9999999L)
-      client.lpush(key, streamId) match {
+      client.pipeline { p =>
+        p.lpush(key, streamId)
+      }.map(_.head).collect {
         case Some(_) => Success(streamId)
-        case None => Failure(new RuntimeException("Redis error"))
-      }
+        case _ => Failure(new RuntimeException("Redis error"))
+      }.getOrElse(Failure(new MaxStreamsException())) //watched key has been altered.
     }
   }
 
   override def stopStream(userId: Long, streamId: Long): Unit = {
+    val client = new RedisClient(redisHost, redisPort)
     client.lrem(s"$keyPrefix$userId", 0, streamId)
   }
 }
